fc4870f8bc2f4d4ea63fef7ce096458e159200f5
Charph <charph.dang@gmail.com>
2022-06-24 22:38:54 +0800

    feat: update lodash flatten,head,findIndex

--- lodash-practice/Array/head.js
+++ lodash-practice/Array/head.js
@@ -4,7 +4,3 @@ const arr = [1,2,3]
 const result = _.head(arr);
 console.log(result) // Charph-log
 console.log(arr) // Charph-log
-
-const result1 = _.first(arr);
-console.log(result1) // Charph-log
-console.log(arr) // Charph-log
--- lodash-practice/readme.md
+++ lodash-practice/readme.md
@@ -2,3 +2,5 @@
 
 1. 了解API，能做到什么事
 2. 着重使用fp(function programming)，深入了解函数式编程
+3. fp 模块下，所有的方法都是immutable的，不会更改源数据
+4. pure function的好处就是，无论方法执行多少次，输入固定，则输出固定
